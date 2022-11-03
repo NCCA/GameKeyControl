6635fde1e13e982b5de2d54458d122569a25d261
jmacey <jmacey@bournemouth.ac.uk>
2022-11-03 19:48:30 +0000

    updated for NGL9 master branch renamed main

--- src/NGLScene.cpp
+++ src/NGLScene.cpp
@@ -7,7 +7,7 @@
 #include <ngl/VAOPrimitives.h>
 #include <ngl/ShaderLib.h>
 
-constexpr static float s_shipUpdate=0.2f;
+constexpr static float s_shipUpdate = 0.2f;
 
 NGLScene::NGLScene()
 {
@@ -17,24 +17,22 @@ NGLScene::NGLScene()
   // this will be triggered at an interval of every 5ms
   // to stop this timer we call killTimer(m_animationTimer) we can have
   // multiple timers but each must have it's own ID
-  m_updateShipTimer=startTimer(2);
-  m_redrawTimer=startTimer(20);
+  m_updateShipTimer = startTimer(2);
+  m_redrawTimer = startTimer(20);
   // now clear the key set
   m_keysPressed.clear();
-
 }
-
 
 NGLScene::~NGLScene()
 {
-  std::cout<<"Shutting down NGL, removing VAO's and Shaders\n";
+  std::cout << "Shutting down NGL, removing VAO's and Shaders\n";
 }
 
-void NGLScene::resizeGL( int _w, int _h )
+void NGLScene::resizeGL(int _w, int _h)
 {
-  m_project=ngl::perspective( 45.0f, static_cast<float>( _w ) / _h, 0.05f, 350.0f );
-  m_win.width  = static_cast<int>( _w * devicePixelRatio() );
-  m_win.height = static_cast<int>( _h * devicePixelRatio() );
+  m_project = ngl::perspective(45.0f, static_cast<float>(_w) / _h, 0.05f, 350.0f);
+  m_win.width = static_cast<int>(_w * devicePixelRatio());
+  m_win.height = static_cast<int>(_h * devicePixelRatio());
 }
 
 void NGLScene::initializeGL()
@@ -43,7 +41,7 @@ void NGLScene::initializeGL()
   // gl commands from the lib, if this is not done program will crash
   ngl::NGLInit::initialize();
 
-  glClearColor(0.0f,0.0f,0.0f,1.0f);			   // black Background
+  glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // black Background
   // enable depth testing for drawing
   glEnable(GL_DEPTH_TEST);
   // enable multisampling for smoother drawing
@@ -51,142 +49,149 @@ void NGLScene::initializeGL()
   // we are creating a shader called Phong
   ngl::ShaderLib::createShaderProgram("Phong");
   // now we are going to create empty shaders for Frag and Vert
-  ngl::ShaderLib::attachShader("PhongVertex",ngl::ShaderType::VERTEX);
-  ngl::ShaderLib::attachShader("PhongFragment",ngl::ShaderType::FRAGMENT);
+  ngl::ShaderLib::attachShader("PhongVertex", ngl::ShaderType::VERTEX);
+  ngl::ShaderLib::attachShader("PhongFragment", ngl::ShaderType::FRAGMENT);
   // attach the source
-  ngl::ShaderLib::loadShaderSource("PhongVertex","shaders/PhongVertex.glsl");
-  ngl::ShaderLib::loadShaderSource("PhongFragment","shaders/PhongFragment.glsl");
+  ngl::ShaderLib::loadShaderSource("PhongVertex", "shaders/PhongVertex.glsl");
+  ngl::ShaderLib::loadShaderSource("PhongFragment", "shaders/PhongFragment.glsl");
   // compile the shaders
   ngl::ShaderLib::compileShader("PhongVertex");
   ngl::ShaderLib::compileShader("PhongFragment");
   // add them to the program
-  ngl::ShaderLib::attachShaderToProgram("Phong","PhongVertex");
-  ngl::ShaderLib::attachShaderToProgram("Phong","PhongFragment");
-
+  ngl::ShaderLib::attachShaderToProgram("Phong", "PhongVertex");
+  ngl::ShaderLib::attachShaderToProgram("Phong", "PhongFragment");
 
   // now we have associated this data we can link the shader
   ngl::ShaderLib::linkProgramObject("Phong");
 
   // and make it active ready to load values
   ngl::ShaderLib::use("Phong");
-  ngl::Vec4 lightPos(0.0f,0.0f,-2.0f,0.0f);
-  ngl::ShaderLib::setUniform("light.position",lightPos);
-  ngl::ShaderLib::setUniform("light.ambient",0.0f,0.0f,0.0f,1.0f);
-  ngl::ShaderLib::setUniform("light.diffuse",1.0f,1.0f,1.0f,1.0f);
-  ngl::ShaderLib::setUniform("light.specular",0.8f,0.8f,0.8f,1.0f);
+  ngl::Vec4 lightPos(0.0f, 0.0f, -2.0f, 0.0f);
+  ngl::ShaderLib::setUniform("light.position", lightPos);
+  ngl::ShaderLib::setUniform("light.ambient", 0.0f, 0.0f, 0.0f, 1.0f);
+  ngl::ShaderLib::setUniform("light.diffuse", 1.0f, 1.0f, 1.0f, 1.0f);
+  ngl::ShaderLib::setUniform("light.specular", 0.8f, 0.8f, 0.8f, 1.0f);
   // gold like phong material
-  ngl::ShaderLib::setUniform("material.ambient",0.274725f,0.1995f,0.0745f,0.0f);
-  ngl::ShaderLib::setUniform("material.diffuse",0.75164f,0.60648f,0.22648f,0.0f);
-  ngl::ShaderLib::setUniform("material.specular",0.628281f,0.555802f,0.3666065f,0.0f);
-  ngl::ShaderLib::setUniform("material.shininess",51.2f);
+  ngl::ShaderLib::setUniform("material.ambient", 0.274725f, 0.1995f, 0.0745f, 0.0f);
+  ngl::ShaderLib::setUniform("material.diffuse", 0.75164f, 0.60648f, 0.22648f, 0.0f);
+  ngl::ShaderLib::setUniform("material.specular", 0.628281f, 0.555802f, 0.3666065f, 0.0f);
+  ngl::ShaderLib::setUniform("material.shininess", 51.2f);
 
   // Now we will create a basic Camera from the graphics library
   // This is a static camera so it only needs to be set once
   // First create Values for the camera position
-  ngl::Vec3 from(0,0,-80);
-  ngl::Vec3 to(0,0,0);
-  ngl::Vec3 up(0,1,0);
+  ngl::Vec3 from(0, 0, -80);
+  ngl::Vec3 to(0, 0, 0);
+  ngl::Vec3 up(0, 1, 0);
   // now load to our new camera
-  m_view=ngl::lookAt(from,to,up);
+  m_view = ngl::lookAt(from, to, up);
   // set the shape using FOV 45 Aspect Ratio based on Width and Height
   // The final two are near and far clipping planes of 0.5 and 10
-  m_project=ngl::perspective(45,720.0f/576.0f,0.05f,350);
-  ngl::ShaderLib::setUniform("viewerPos",from);
+  m_project = ngl::perspective(45, 720.0f / 576.0f, 0.05f, 350);
+  ngl::ShaderLib::setUniform("viewerPos", from);
   // create our spaceship
-  m_ship= std::make_unique<SpaceShip>(ngl::Vec3(0,0,0),"models/SpaceShip.obj");
+  m_ship = std::make_unique<SpaceShip>(ngl::Vec3(0, 0, 0), "models/SpaceShip.obj");
 }
-
 
 void NGLScene::loadMatricesToShader()
 {
-  ngl::Mat4 MVP=m_project*m_view  ;
-  ngl::ShaderLib::setUniform("MVP",MVP);
- }
+  ngl::Mat4 MVP = m_project * m_view;
+  ngl::ShaderLib::setUniform("MVP", MVP);
+}
 
 void NGLScene::paintGL()
 {
   // clear the screen and depth buffer
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   // now load these values to the shader
-  m_ship->draw("Phong",m_view,m_project);
+  m_ship->draw("Phong", m_view, m_project);
 }
 
 //----------------------------------------------------------------------------------------------------------------------
-void NGLScene::mouseMoveEvent( QMouseEvent* _event )
+void NGLScene::mouseMoveEvent(QMouseEvent *_event)
 {
-  // note the method buttons() is the button state when event was called
-  // that is different from button() which is used to check which button was
-  // pressed when the mousePress/Release event is generated
-  if ( m_win.rotate && _event->buttons() == Qt::LeftButton )
+// note the method buttons() is the button state when event was called
+// that is different from button() which is used to check which button was
+// pressed when the mousePress/Release event is generated
+#if QT_VERSION > QT_VERSION_CHECK(6, 0, 0)
+  auto position = _event->position();
+#else
+  auto position = _event->pos();
+#endif
+  if (m_win.rotate && _event->buttons() == Qt::LeftButton)
   {
-    int diffx = _event->x() - m_win.origX;
-    int diffy = _event->y() - m_win.origY;
-    m_win.spinXFace += static_cast<int>( 0.5f * diffy );
-    m_win.spinYFace += static_cast<int>( 0.5f * diffx );
-    m_win.origX = _event->x();
-    m_win.origY = _event->y();
+    int diffx = position.x() - m_win.origX;
+    int diffy = position.y() - m_win.origY;
+    m_win.spinXFace += static_cast<int>(0.5f * diffy);
+    m_win.spinYFace += static_cast<int>(0.5f * diffx);
+    m_win.origX = position.x();
+    m_win.origY = position.y();
     update();
   }
   // right mouse translate code
-  else if ( m_win.translate && _event->buttons() == Qt::RightButton )
+  else if (m_win.translate && _event->buttons() == Qt::RightButton)
   {
-    int diffX      = static_cast<int>( _event->x() - m_win.origXPos );
-    int diffY      = static_cast<int>( _event->y() - m_win.origYPos );
-    m_win.origXPos = _event->x();
-    m_win.origYPos = _event->y();
+    int diffX = static_cast<int>(position.x() - m_win.origXPos);
+    int diffY = static_cast<int>(position.y() - m_win.origYPos);
+    m_win.origXPos = position.x();
+    m_win.origYPos = position.y();
     m_modelPos.m_x += INCREMENT * diffX;
     m_modelPos.m_y -= INCREMENT * diffY;
     update();
   }
 }
 
-
 //----------------------------------------------------------------------------------------------------------------------
-void NGLScene::mousePressEvent( QMouseEvent* _event )
+void NGLScene::mousePressEvent(QMouseEvent *_event)
 {
-  // that method is called when the mouse button is pressed in this case we
-  // store the value where the maouse was clicked (x,y) and set the Rotate flag to true
-  if ( _event->button() == Qt::LeftButton )
+// that method is called when the mouse button is pressed in this case we
+// store the value where the maouse was clicked (x,y) and set the Rotate flag to true
+#if QT_VERSION > QT_VERSION_CHECK(6, 0, 0)
+  auto position = _event->position();
+#else
+  auto position = _event->pos();
+#endif
+  if (_event->button() == Qt::LeftButton)
   {
-    m_win.origX  = _event->x();
-    m_win.origY  = _event->y();
+    m_win.origX = position.x();
+    m_win.origY = position.y();
     m_win.rotate = true;
   }
   // right mouse translate mode
-  else if ( _event->button() == Qt::RightButton )
+  else if (_event->button() == Qt::RightButton)
   {
-    m_win.origXPos  = _event->x();
-    m_win.origYPos  = _event->y();
+    m_win.origXPos = position.x();
+    m_win.origYPos = position.y();
     m_win.translate = true;
   }
 }
 
 //----------------------------------------------------------------------------------------------------------------------
-void NGLScene::mouseReleaseEvent( QMouseEvent* _event )
+void NGLScene::mouseReleaseEvent(QMouseEvent *_event)
 {
   // that event is called when the mouse button is released
   // we then set Rotate to false
-  if ( _event->button() == Qt::LeftButton )
+  if (_event->button() == Qt::LeftButton)
   {
     m_win.rotate = false;
   }
   // right mouse translate mode
-  if ( _event->button() == Qt::RightButton )
+  if (_event->button() == Qt::RightButton)
   {
     m_win.translate = false;
   }
 }
 
 //----------------------------------------------------------------------------------------------------------------------
-void NGLScene::wheelEvent( QWheelEvent* _event )
+void NGLScene::wheelEvent(QWheelEvent *_event)
 {
 
   // check the diff of the wheel position (0 means no change)
-  if ( _event->angleDelta().x() > 0 )
+  if (_event->angleDelta().x() > 0)
   {
     m_modelPos.m_z += ZOOM;
   }
-  else if ( _event->angleDelta().x() < 0 )
+  else if (_event->angleDelta().x() < 0)
   {
     m_modelPos.m_z -= ZOOM;
   }
@@ -194,77 +199,95 @@ void NGLScene::wheelEvent( QWheelEvent* _event )
 }
 //----------------------------------------------------------------------------------------------------------------------
 
-void NGLScene::timerEvent( QTimerEvent *_event)
+void NGLScene::timerEvent(QTimerEvent *_event)
 {
-	// the usual process is to check the event timerID and compare it to
-	// any timers we have started with startTimer
-	if (_event->timerId() == m_updateShipTimer)
-	{
-		moveShip();
-	}
-	if (_event->timerId() == m_redrawTimer)
-	{
-		update();
-	}
-
+  // the usual process is to check the event timerID and compare it to
+  // any timers we have started with startTimer
+  if (_event->timerId() == m_updateShipTimer)
+  {
+    moveShip();
+  }
+  if (_event->timerId() == m_redrawTimer)
+  {
+    update();
+  }
 }
 
 void NGLScene::moveShip()
 {
-	/// first we reset the movement values
-	float xDirection=0.0;
-	float yDirection=0.0;
-	float rotation=0.0;
-	// now we loop for each of the pressed keys in the the set
-	// and see which ones have been pressed. If they have been pressed
-	// we set the movement value to be an incremental value
-	foreach(Qt::Key key, m_keysPressed)
-	{
-		switch (key)
-		{
-			case Qt::Key_Left :  { xDirection=s_shipUpdate; break;}
-			case Qt::Key_Right : { xDirection=-s_shipUpdate; break;}
-			case Qt::Key_Up :		 { yDirection=s_shipUpdate; break;}
-			case Qt::Key_Down :  { yDirection=-s_shipUpdate; break;}
-			case Qt::Key_R :     { rotation=1.0; break;}
-			default : break;
-		}
-	}
-	// if the set is non zero size we can update the ship movement
-	// then tell openGL to re-draw
-	if(m_keysPressed.size() !=0)
-	{
-		m_ship->move(xDirection,yDirection);
-		m_ship->rotate(rotation);
-	}
+  /// first we reset the movement values
+  float xDirection = 0.0;
+  float yDirection = 0.0;
+  float rotation = 0.0;
+  // now we loop for each of the pressed keys in the the set
+  // and see which ones have been pressed. If they have been pressed
+  // we set the movement value to be an incremental value
+  foreach (Qt::Key key, m_keysPressed)
+  {
+    switch (key)
+    {
+    case Qt::Key_Left:
+    {
+      xDirection = s_shipUpdate;
+      break;
+    }
+    case Qt::Key_Right:
+    {
+      xDirection = -s_shipUpdate;
+      break;
+    }
+    case Qt::Key_Up:
+    {
+      yDirection = s_shipUpdate;
+      break;
+    }
+    case Qt::Key_Down:
+    {
+      yDirection = -s_shipUpdate;
+      break;
+    }
+    case Qt::Key_R:
+    {
+      rotation = 1.0;
+      break;
+    }
+    default:
+      break;
+    }
+  }
+  // if the set is non zero size we can update the ship movement
+  // then tell openGL to re-draw
+  if (m_keysPressed.size() != 0)
+  {
+    m_ship->move(xDirection, yDirection);
+    m_ship->rotate(rotation);
+  }
 }
 //----------------------------------------------------------------------------------------------------------------------
 // in this case this method is called from the MainWindow class and passed
 // the key event from that, if we didn't need to process the events in main
 // window we could just overload this as the keyPressEvent and only process here.
 //----------------------------------------------------------------------------------------------------------------------
-void NGLScene::keyPressEvent( QKeyEvent *_event)
+void NGLScene::keyPressEvent(QKeyEvent *_event)
 {
-	// add to our keypress set the values of any keys pressed
-	m_keysPressed += (Qt::Key)_event->key();
+  // add to our keypress set the values of any keys pressed
+  m_keysPressed += (Qt::Key)_event->key();
 
   switch (_event->key())
   {
-    case Qt::Key_Escape : QApplication::exit(EXIT_SUCCESS); break;
+  case Qt::Key_Escape:
+    QApplication::exit(EXIT_SUCCESS);
+    break;
   }
-
 }
-
 
 //----------------------------------------------------------------------------------------------------------------------
 // in this case this method is called from the MainWindow class and passed
 // the key event from that, if we didn't need to process the events in main
 // window we could just overload this as the keyPressEvent and only process here.
 //----------------------------------------------------------------------------------------------------------------------
-void NGLScene::keyReleaseEvent( QKeyEvent *_event	)
+void NGLScene::keyReleaseEvent(QKeyEvent *_event)
 {
-	// remove from our key set any keys that have been released
-	m_keysPressed -= (Qt::Key)_event->key();
+  // remove from our key set any keys that have been released
+  m_keysPressed -= (Qt::Key)_event->key();
 }
-
-
